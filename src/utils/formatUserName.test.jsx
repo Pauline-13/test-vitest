15c0f606fe9b81e11f4c733fff8aba095c2346dc
Pauline_13 <pauline.tincq@yahoo.fr>
2025-07-07 10:54:22 +0200

    premier test

---
+++ src/utils/formatUserName.test.jsx
@@ -0,0 +1,9 @@
+import { describe, it, expect } from "vitest";
+import { formatUserName } from "./formatUserName";
+
+describe("formatUserName", () => {
+  it("formate correctement un nom et prénom", () => {
+    const result = formatUserName("alice", "dupont");
+    expect(result).toBe("Alice Dupont");
+  });
+});
