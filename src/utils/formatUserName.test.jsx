0b8f09bea1775125a60f11379a423965b097b536
Pauline_13 <pauline.tincq@yahoo.fr>
2025-07-07 16:27:43 +0200

    terminé

--- src/utils/formatUserName.test.jsx
+++ src/utils/formatUserName.test.jsx
@@ -8,3 +8,27 @@ describe("formatUserName", () => {
   });
 });
 
+
+describe("formatUserName", () => {
+  it("supprime les espaces superflus autour du prénom et du nom", () => {
+    const result = formatUserName("   alice   ", "dupont");
+    expect(result).toBe("Alice Dupont");
+  });
+});
+
+
+describe("formatUserName", () => {
+  it("lève une erreur si le prénom est manquant", () => {
+  expect(() => formatUserName("", "Dupont")).toThrow("Les deux champs sont requis");
+});
+
+it("lève une erreur si le nom est manquant", () => {
+  expect(() => formatUserName("Alice", "")).toThrow("Les deux champs sont requis");
+});
+});
+
+
+it("met correctement en majuscule la première lettre du prénom et du nom, quel que soit la casse d'origine", () => {
+  const result = formatUserName("aLiCe", "dUpoNT");
+  expect(result).toBe("Alice Dupont");
+});
